e9a99fd5c7843d59f93b6e714255bac831d886c1
KathTheDragon <kaththedragon@gmail.com>
2020-10-02 21:30:03 +0100

    Add basic UI

---
+++ src/client.py
@@ -0,0 +1,15 @@
+from . import ui
+
+class Client:
+    def __init__(self):
+        self.window, self.output, self.input, self.tabbar = ui.create()
+        self.set_title()
+    
+    def set_title(self, player='', puppet=''):
+        if not player:
+            self.root.title('Spindizzy')
+        elif not puppet:
+            self.root.title(f'{player} - Spindizzy')
+        else:
+            self.root.title(f'{puppet} - {player} - Spindizzy')
+            
---
+++ src/ui.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import ttk
+
+def create():
+    # Set up window
+    window = tk.Tk()
+    create_menus(root)
+    
+    # Create widgets
+    output = tk.Text(window, wrap='word', state='disabled')
+    output_scroll = ttk.Scrollbar(window, orient=tk.VERTICAL, command=output.yview)
+    output.configure(yscrollcommand=output_scroll.set)
+    input = tk.Text(window, height=1, wrap='word')
+    tabbar = ttk.Frame(window, height=16)
+    
+    # Grid widgets
+    output.grid(column=0, row=0, sticky='nwes')
+    output_scroll.grid(column=1, row=0, sticky='nwes')
+    input.grid(column=0, row=1, columnspan=2, sticky='nwes')
+    tabbar.grid(column=0, row=2, columnspan=2, sticky='nwes')
+    
+    # Configure for resizing
+    window.columnconfigure(0, weight=1)
+    window.rowconfigure(0, weight=1)
+    
+    # Final actions
+    input.focus()
+    
+    return window, output, input, tabbar
+
+def create_menus(root):
+    root.option_add('*tearOff', False)
+    menubar = tk.Menu(root)
+    root['menu'] = menubar
+    file = tk.Menu(menubar)
+    menubar.add_cascade(menu=file, label='File')
+
+def add_tab(tabbar, player, puppet=''):
+    pass
