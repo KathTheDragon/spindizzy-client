8ecade2d85a3e741b05c562e704c8c9eb2c9b902
KathTheDragon <kaththedragon@gmail.com>
2020-10-07 10:55:35 +0100

    Lookup connections by player rather than by index
    Given that all connections are associated with a player, it makes more sense to access them by that player rather than by a meaningless index

--- src/client.py
+++ src/client.py
@@ -3,10 +3,9 @@ from . import ui, network
 class Client:
     def __init__(self):
         self.window, self.output, self.input, self.tabbar = ui.create()
+        self.connections = {}
         self.active_tab = None
         self.set_title()
-        self.connections = []
-        self.active_conn = None
 
     def set_title(self, player='', puppet=''):
         if not player:
@@ -16,41 +15,26 @@ class Client:
         else:
             self.window.title(f'{puppet} - {player} - Spindizzy')
 
-    def get_connection(self, index):
-        try:
-            return self.connections[index]
-        except IndexError:
-            return None
+    def get_connection(self, player):
+        return self.connections.get(player, None)
 
-    def set_active_conn(self, index):
-        conn = self.get_connection(index)
-        if conn is not None:
-            self.active_conn = conn
+    def new_connection(self, player, password):
+        if player not in self.connections:
+            self.connections[player] = network.Connection(player, password)
 
-    def clear_active_conn(self):
-        self.active_conn = None
+    def delete_connection(self, player):
+        if player in self.connections:
+            del self.connections[player]
 
-    def new_connection(self, player='', password=''):
-        index = len(self.connections)
-        self.connections.append(network.Connection(player, password))
-        self.set_active_conn(-1)
-        return index
-
-    def open_connection(self, index):
-        conn = self.get_connection(index)
+    def open_connection(self, player):
+        conn = self.get_connection(player)
         if conn is not None and not conn.isopen:
             conn.open()
 
-    def close_connection(self, index):
-        conn = self.get_connection(index)
+    def close_connection(self, player):
+        conn = self.get_connection(player)
         if conn is not None and conn.isopen:
             conn.close()
-
-    def delete_connection(self, index):
-        try:
-            self.connections[index] = None
-        except IndexError:
-            pass
 
     def send(self, message):
         if self.active_conn.isopen:
