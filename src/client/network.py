357a435d4c965fadeeb5b8ace788627b42ad19a3
KathTheDragon <kaththedragon@gmail.com>
2021-02-07 18:45:25 +0000

    Implement threading in client.network

--- src/client/network.py
+++ src/client/network.py
@@ -1,5 +1,7 @@
 import socket
 import ssl
+from threading import Thread
+from collections import deque
 
 _context = ssl.create_default_context()
 
@@ -13,33 +15,39 @@ class Connection:
     def __init__(self, name, password):
         self.login = (name, password)
         self.isopen = False
-        self.sent = []
-        self.received = []
+        self.buffer = deque()
 
     def send(self, *messages):
         if not self.isopen:
             raise ConnectionClosed()
-        self.sent.extend(messages)
         self.socket.sendall(('\r\n'.join(messages) + '\r\n').encode())
 
-    def receive(self):
+    def recv(self):
         if not self.isopen:
             raise ConnectionClosed()
-        data = bytearray()
-        try:
-            while True:
-                received = self.socket.recv(4096)
-                if received:
-                    data += received
-                else:
-                    self.close()
-                    break
-        except socket.timeout:
-            pass
-        finally:
+        while self.isopen:
+            data = bytearray()
+            try:
+                while True:
+                    received = self.socket.recv(4096)
+                    if received:
+                        data += received
+                    else:
+                        self.isopen = False
+                        break
+            except socket.timeout:
+                pass
             messages = data.decode().splitlines()
-            self.received.extend(messages)
-            return messages
+            # Timestamp
+            self.buffer.extend(messages)
+        self.socket.close()
+
+    def receive(self):
+        while True:
+            try:
+                yield self.buffer.popleft()
+            except IndexError:
+                break
 
     def open(self):
         if self.isopen:
@@ -49,10 +57,13 @@ class Connection:
             socket.create_connection(('muck.spindizzy.org', 7073), 1),
             server_hostname='muck.spindizzy.org'
         )
+        self.thread = Thread(target=self.recv)
+        self.thread.start()
         self.send('connect {} {}'.format(*self.login))
 
     def close(self):
         if not self.isopen:
             raise ConnectionClosed()
         self.isopen = False
+        self.thread.join()
         self.socket.close()
